f25e73a9a94812147fdfdf67b58ee4368856f80e
Nathan Ladd <nathanladd@gmail.com>
2020-07-14 15:06:17 -0500

    Automated test suite is repaired

--- test/automated.rb
+++ test/automated.rb
@@ -2,5 +2,5 @@ require_relative './test_init'
 
 TestBench::Run.(
   'test/automated',
-  exclude_pattern: %r{/skip\.|(?:_init\.rb|\.sketch\.rb|_sketch\.rb|\.skip\.rb)\z|_integration}
+  exclude_file_pattern: %r{/skip\.|(?:_init\.rb|\.sketch\.rb|_sketch\.rb|\.skip\.rb)\z|_integration}
 ) or exit 1
