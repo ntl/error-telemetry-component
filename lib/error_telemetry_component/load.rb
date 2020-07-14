00a99fd857aa41f642909e2ba06fdedfd0837145
Nathan Ladd <nathanladd@gmail.com>
2020-07-14 16:42:07 -0500

    Superfluous file is removed

--- lib/error_telemetry_component/load.rb
+++ lib/error_telemetry_component/load.rb
@@ -10,5 +10,3 @@ require 'error_telemetry_component/messages/commands/record'
 
 require 'error_telemetry_component/host_info'
 require 'error_telemetry_component/record'
-
-require 'error_telemetry_component/record'
